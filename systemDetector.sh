2279061b16cd94cd6595d5284da79c8f361e7a8d
HasakyOsaky <132791985+HasakyOsaky@users.noreply.github.com>
2025-03-01 22:36:16 +0000

    Add files via upload

--- systemDetector.sh
+++ systemDetector.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
 function ctrl_c(){
-  echo -e "\n[!] Saliendo..."
+  echo -e "\n${redColour}[!] Saliendo...${endColour}"
   tput cnorm
   exit 1
 }
@@ -11,7 +21,7 @@ trap ctrl_c INT
 
 # Validación de argumentos
 if [ -z "$1" ]; then
-  echo -e "\n[+] Uso: ./whichSystem.sh (ip)"
+  echo -e "\n${blueColour}[+]${endColour} ${turquoiseColour}Uso: ./whichSystem.sh \"ip\"${endColour}"
   exit 1
 fi
 
@@ -22,18 +32,19 @@ ttl=$(ping -c 1 $machine_ip | awk '{print $6}' | tr '=' ' ' | grep -v "of" | awk
 
 
 if [ -z "$ttl" ]; then
-  echo -e "\n[+] No obtuvimos TTL de la máquina $machine_ip"
+  echo -e "\n${redColour}[-] No obtuvimos TTL de la máquina $machine_ip ${endColour}"
   tput cnorm
   exit 1
 fi
 
 # Determinar el sistema operativo según el TTL
+
 if [ "$ttl" -le 64 ]; then
-  echo -e "\n[+] $machine_ip -> Linux"
+  echo -e "\n${greenColour}[+]${endColour} ${purpleColour}$machine_ip -> Linux${endColour}"
 elif [ "$ttl" -gt 64 ] && [ "$ttl" -le 128 ]; then
-  echo -e "\n[+] $machine_ip -> Windows"
+  echo -e "\n${greenColour}[+]${endColour} ${purpleColour}$machine_ip -> Windows${endColour}"
 else
-  echo -e "\n[-] No se pudo determinar el sistema operativo"
+  echo -e "\n${redColour}[-] No se pudo determinar el sistema operativo${endColour}"
 fi
 
 tput cnorm
